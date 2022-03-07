9c15dfa904ddbb8c5bda3c6822680062d20d11f7
sy.nguyen <syn282002@gmail.com>
2022-03-07 10:14:20 +0700

    merge setting

--- src/Navigations/router/routes.js
+++ src/Navigations/router/routes.js
@@ -1,14 +1,38 @@
 import AccountScreen from '../../Screens/AccountScreen';
 import MapScreen from '../../Screens/MapScreen';
 import SocietyGreen from '../../Screens/SocietyGreen';
+import { FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 import HomePageStack from '../HomePageStack';
 import CarouselMap from '../../Screens/HomePageScreen/CarouselMap';
+import { Text } from 'react-native';
+import { colors } from '../../global/styles';
 const bottomRoutes = [
   {
     name: 'HomeScreen',
     component: HomePageStack,
     title: 'Trang chủ',
     icon: 'home',
+    options: {
+      headerShown: false,
+      tabBarLabel: ({ color, size, focused }) => (
+        <Text
+          style={{
+            color: focused ? 'rgb(18, 136, 58)' : colors.grey2,
+            fontSize: 13,
+            fontWeight: focused ? 'bold' : 'normal',
+          }}
+        >
+          Trang chủ
+        </Text>
+      ),
+      tabBarIcon: ({ color, size, focused }) => (
+        <FontAwesome5
+          name="home"
+          size={size}
+          color={focused ? 'rgb(18, 136, 58)' : colors.grey2}
+        />
+      ),
+    },
   },
   {
     name: 'MapScreen',
